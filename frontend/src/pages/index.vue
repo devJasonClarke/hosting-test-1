50783fd6f6381ca50ed2f22bf570f2b51b039307
devJasonClarke <jettison200@gmail.com>
2022-01-06 19:44:21 -0500

    ⚡first commit⚡

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <TheNav />
+  <div>
+    <img src="@/assets/logo.png" alt="why are you running" />
+    <h1>Welcome to the home view</h1>
+    <router-link to="/about">about</router-link>
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {}
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Hosting test 1
+
+A simple project to test how easy it is to host separate projects from multiple folders in the same repo
